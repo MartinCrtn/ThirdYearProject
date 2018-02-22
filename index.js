35155b3ef7f9c6c1473c37644cd7426af39af691
Martin Courtinat <martincourtinat@MacBook-Pro-de-Martin.local>
2018-02-22 21:40:34 +0000

    Update before delete

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import { Navigation } from 'react-native-navigation';
 
-AppRegistry.registerComponent('ThirdYearProject', () => App);
+import { registerScreens } from './screens';
+
+registerScreens(); // this is where you register all of your app's screens
+
+// start the app
+Navigation.startTabBasedApp({
+  tabs: [
+    {
+      label: 'One',
+      screen: 'Screen1',
+      icon: require('./images/icon1.png'),
+      selectedIcon: require('./images/icon1_selected.png'),
+      title: 'Screen One'
+    },
+    {
+      label: 'Two',
+      screen: 'Screen2',
+      icon: require('./images/icon2.png'),
+      selectedIcon: require('./images/icon2_selected.png'),
+      title: 'Screen Two'
+    }
+  ]
+});
